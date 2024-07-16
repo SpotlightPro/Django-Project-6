14a2cac26a72ba482790b097f9b55edc3fc2a2c6
SpotlightPro <117112240+SpotlightPro@users.noreply.github.com>
2024-07-16 03:26:29 +0000

    Day 2: Members database and form created and works fine
    Day 2: Members database and form created and works fine

--- djangodb/webapp/views.py
+++ djangodb/webapp/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Member
+from .forms import MemberForm
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
     all_members = Member.objects.all
-    return render(request, 'home.html', {'all':all_members})
+    return render(request, 'home.html', {'all': all_members})
+
+
+def join(request):
+    if request.method == "POST":
+        form = MemberForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        else:
+            messages.success(request, 'There was an error in your form. Please try again...')
+            return redirect('join')
+        messages.success(request, 'Your form has been Submitted Successfully!')
+        return redirect('home')
+    else:
+        return render(request, 'join.html', {})
